0604ac58c97a0e001f59ba85ddae836b34edf982
inaciosalvador <inaciosalvador03@gmail.com>
2022-11-09 07:21:42 -0300

    Exercícios Repetição

---
+++ Repetição/exercicio29.java
@@ -0,0 +1,13 @@
+/*) Faça um programa que exiba os números de 1 até 2000.*/
+
+public class exercicio29 {
+
+	public static void main(String[] args) {
+		int cont = 0;
+		while (cont <= 2000) {
+			System.out.println(cont);
+			cont++;
+		}
+	}
+
+}
